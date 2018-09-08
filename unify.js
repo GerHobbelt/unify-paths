dc494d62755cb3ff34cc090ad98492706ae6e54d
Ger Hobbelt <ger@hobbelt.com>
2018-09-08 19:00:18 +0200

    - added package.json npm package definition - introduced API - added unit tests - added babel-based build process to produce an Node6/classic library file in /dist/ while the modern code resides in `/unify.js` - all tests pass

---
+++ unify.js
@@ -0,0 +1,89 @@
+
+/**
+ * Used to match `RegExp`
+ * [syntax characters](http://ecma-international.org/ecma-262/7.0/#sec-patterns).
+ */
+const reRegExpChar = /[\\^$.*+?()[\]{}|]/g;
+
+/**
+ * Escapes the `RegExp` special characters "^", "$", "\", ".", "*", "+",
+ * "?", "(", ")", "[", "]", "{", "}", and "|" in `string`.
+ *
+ * Taken/derived from lodash
+ */
+function escapeRegExp(string) {
+  return string.replace(reRegExpChar, '\\$&');
+}
+
+// module.exports = unify;
+
+// Regex for substitution in path
+const baseReStr = (/(^|[^\w_\\/:])((?!http|s?ftp|uri)\w+:)?[\\/]+([\w_\\/-]+?[\\/]+)?X[\\/]+([\w_\\/-]+)/).source;
+console.error('baseReStr:', baseReStr);
+
+// Regex for generic path recognition
+const genericAbsReStr = /(^|[^\w_\\/:])((?!http|s?ftp|uri)\w+:)?([\\/]+[\w_-]+[\\/]+)([\w_\\/-]+)/g;
+
+export default function unify(input, options = {}) {
+	let text = '' + input;
+
+	if (options.hasExplicitEscapes) {
+    	text = text.replace(/(?=\\[^uxwbn])\\\\?/g, "/");
+    } else {
+    	text = text.replace(/\\/g, "/");
+    }
+    console.error("phase 1:", {input, text, options});
+
+	let cwdPathPrefix = (options.cwdPathPrefix || '').replace(/\\/g, '/');
+	let cwdReplacement = "<CWD>";
+
+	while (cwdPathPrefix) {
+		let re = RegExp(escapeRegExp(cwdPathPrefix), "g");
+		text = text.replace(re, cwdReplacement);
+
+	    // execute the equivalent of path.dirname(), without requiring the `path` module:
+	    cwdPathPrefix = cwdPathPrefix.replace(/\/[^/]+$/, '');
+	    cwdReplacement += '/..';
+	    if (cwdPathPrefix.indexOf('/') < 0) break;
+	}
+    console.error("phase 0:", {input, text, options});
+
+    if (options.reducePaths) {
+    	let arr = Array.isArray(options.reducePaths) ? options.reducePaths : [options.reducePaths];
+    	for (let i = 0, len = arr.length; i < len; i++) {
+    		let el = arr[i];
+    		if (!el) continue;
+
+    		// decode spec item: {id, replacement}
+    		let id, replacement;
+    		if (el.id) {
+    			id = el.id;
+    			replacement = el.replacement;
+    			if (replacement == null) replacement = id;
+    		} else {
+    			id = '' + el;
+    			replacement = id;
+    		}
+
+			let re = RegExp(baseReStr.replace('X', escapeRegExp(id)), "g");
+			console.error('RE:', re);
+	    	text = text.replace(re, function (m, prefix, uri, path1, path2) {
+	    		// discard drive letters, e.g. "C:/", but do not delete network IDLs, e.g. "Waterloo:/":
+	    		if (!uri || uri.length <= 2) uri = ""; 
+				return `${prefix}${uri}/${replacement}/${path2.replace(/\\/g, '/')}`;
+			});
+		}
+	    console.error("phase 2:", {input, text, options});
+    }
+
+	let re = genericAbsReStr;
+	console.error('RE:', re);
+	text = text.replace(re, function (m, prefix, uri, path1, path2) {
+		// discard drive letters, e.g. "C:/", but do not delete network IDLs, e.g. "Waterloo:/":
+		if (!uri || uri.length <= 2) uri = ""; 
+		return `${prefix}${uri}${path1.replace(/\\/g, '/')}${path2.replace(/\\/g, '/')}`;
+	});
+    console.error("phase 4:", {input, text, options});
+
+	return text;
+}
